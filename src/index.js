40d542d3a7a6febc1557db324b24e0b2484a2e02
CPU <santiago.amoros@estudiantat.upc.edu>
2023-08-01 23:02:34 +0200

    Initial commit from branch "introduction"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,115 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function Greeting() {
+//   return <h2> My first component</h2>
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function Greeting(){
+//   return React.createElement('h2',{},'hello world')
+// }
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2> My first component</h2>
+//     </div>
+//   );
+// }
+
+// function Greeting(){
+//   return React.createElement('div',{},React.createElement('h3',{}, 'My first COmponent'))
+// }
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2> My first component</h2>
+//       <ul>
+//         <li><a href="#" target="_blank" rel="noopener noreferrer">Hello World</a></li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// The returned JSX MUST BE A UNIQUE ELEMENT, you can go as complex and crazy as you
+//want but WITHIN that JSX element!!!
+//If you try the following, it wont work :(!!!...
+//And of course the editor says:
+// 'JSX expressions must have one parent element'
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2> My first component</h2>
+//     </div>
+//     <h1>hello</h1>
+//   );
+// }
+//...put a unique parent element and problem solved! :)
+
+// function Greeting() {
+//   return (
+//     <section>
+//       <div>
+//         <div>
+//           <h2> My first component</h2>
+//         </div>
+//         <h1>hello</h1>
+//       </div>
+//       <h2>Cool section</h2>
+//     </section>
+//   );
+// }
+
+//you can also go with ...
+
+// function Greeting() {
+//   return (
+
+//     <React.Fragment>
+//       <div>
+//         <div>
+//           <h2> My first component</h2>
+//         </div>
+//         <h1>hello</h1>
+//       </div>
+//       <h2 className="RedCoolText">Cool section</h2>
+//     </React.Fragment>
+
+//   );
+//}
+
+// The parenthesis in the return is not necessary is there to help us.
+//If we did not have it...this does not work
+
+// function Greeting() {
+//   return 
+//     <React.Fragment>
+//       <div>
+//         <div>
+//           <h2> My first component</h2>
+//         </div>
+//         <h1>hello</h1>
+//       </div>
+//       <h2 className="RedCoolText">Cool section</h2>
+//     </React.Fragment>
+//   ;
+// }
+
+//Without parentesis we need that the opening of the first element stays on the same
+//line as the return statement :)
+function Greeting() {
+  return <React.Fragment>
+      <div>
+        <div>
+          <h2> My first component</h2>
+        </div>
+        <h1>hello</h1>
+      </div>
+      <h2 className="RedCoolText">Cool section</h2>
+    </React.Fragment>
+  ;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greeting />);
---
+++ src/testing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const testing = () => {
+  return (
+    <div>testing</div>
+  )
+}
+
+import React from 'react'
+
+export default function testing() {
+  return (
+    <div>testing</div>
+  )
+}
+
